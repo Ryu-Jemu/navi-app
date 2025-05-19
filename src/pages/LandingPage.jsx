1a624b69650b7fbe69fad6460b12ac5481a28709
Maeng-nine <decemryu77@gmail.com>
2025-05-19 15:09:59 +0900

    Finish Landing Page: layout, font, and color styles applied

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <h1 className="logo-text">Navi</h1>
+      <p className="sub-text">Socyan</p>
+    </div>
+  );
+};
+
+export default LandingPage;

463a473f761985986843a648efd545274bfbb4b7
Maeng-nine <decemryu77@gmail.com>
2025-05-24 14:33:32 +0900

    Modify File Page: color and Icon

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import './BoardPage.css';
 import BottomNav from '../BottomNav/BottomNav';
+import LogoImage from '../assets/logo.png';
 import SearchIcon from '../assets/Search_Icon.png';
 import ProfileIcon from '../assets/Profile_Icon.png';
-import PostIcon from '../assets/List_Icon.png';
+import FileImage from '../assets/File_Icon.png';
+
 
 const BoardPage = () => {
   return (
     <div className="board-page">
       <header className="board-header">
-        <span className="logo-text">Navi</span>
-        <span className="board-title">Board</span>
+        <img src={LogoImage} alt="Logo" className="logo" />
         <div className="header-icons">
           <img src={SearchIcon} alt="Search" />
           <img src={ProfileIcon} alt="Profile" />
         </div>
       </header>
+
+      <h2 className="board-title">Community</h2>
 
       <div className="language-tabs">
         <span className="tab active">English</span>
@@ -24,20 +27,19 @@ const BoardPage = () => {
 
       <div className="board-menu">
         <div className="menu-item">
-          <img src={PostIcon} alt="My Posts" />
+          <img src={FileImage} alt="My Posts" />
           <span>My Posts</span>
         </div>
         <div className="menu-item">
-          <img src={PostIcon} alt="Commented" />
+          <img src={FileImage} alt="Commented" />
           <span>Commented</span>
         </div>
         <div className="menu-item">
-          <img src={PostIcon} alt="Scrap" />
+          <img src={FileImage} alt="Scrap" />
           <span>Scrap</span>
         </div>
-        <hr className="divider" />
         <div className="menu-item">
-          <img src={PostIcon} alt="Free Board" />
+          <img src={FileImage} alt="Free Board" />
           <span>Free Board</span>
         </div>
       </div>
